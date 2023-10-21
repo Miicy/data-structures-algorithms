e429b08a90924ad9e76aeb1d2362c92b4d586868
Micy <100793297+Miicy@users.noreply.github.com>
2023-10-21 19:00:51 +0200

    recursion

---
+++ Recursion/FindFactorial.js
@@ -0,0 +1,11 @@
+//faktorijel je matematička funkcija kojom se izračunava proizvod prirodnih brojeva od 1 do nekog određenog prirodnog broja n
+
+function findFactorialRecursion(){
+    //recursion
+
+}
+
+function findFactorialIterative(){
+    //loop
+
+}
